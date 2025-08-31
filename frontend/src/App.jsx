2a979e2e4978c5747a5d1a3e57fa932a0173565f
Gurucharan M <maripalagurucharan@gmail.com>
2025-08-31 17:38:48 +0530

    fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
   return (
     <>
-        {/* <Navbar/> */}
+        <Navbar/>
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/create" element={<Create/>}/>
