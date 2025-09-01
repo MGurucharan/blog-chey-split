d2c26d0aa4fc2e0ea066ab2363b5fc65a18f5e09
Gurucharan M <maripalagurucharan@gmail.com>
2025-09-01 19:31:33 +0530

    connected mongoDB

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,12 @@ const upload = multer({
 });
 
 //connects server to MongoDB
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ Connected to MongoDB Atlas"))
+.catch((err) => console.error("❌ MongoDB connection error:", err));
 
 
 
